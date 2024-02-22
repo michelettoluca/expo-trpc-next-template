6d476fcfa0ef837b59f1f21c3f7d0c6ec544b630
Luca Micheletto <mch.luca00@gmail.com>
2024-02-22 18:17:44 +0100

    Working tRPC

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
+
+import { createTRPCReact } from "@trpc/react-query";
+import type { AppRouter } from "server/src/index";
+
+export const trpc = createTRPCReact<AppRouter>();
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+import React, { useState } from "react";
+
+export default function App() {
+	const [queryClient] = useState(() => new QueryClient());
+	const [trpcClient] = useState(() =>
+		trpc.createClient({
+			links: [
+				httpBatchLink({
+					url: "http://192.168.1.12:4000/trpc",
+				}),
+			],
+		})
+	);
+	return (
+		<trpc.Provider client={trpcClient} queryClient={queryClient}>
+			<QueryClientProvider client={queryClient}>
+				<Aa />
+			</QueryClientProvider>
+		</trpc.Provider>
+	);
+}
+
+function Aa() {
+	const asd = trpc.getUser.useQuery("asd");
+
+	return <Text>{JSON.stringify(asd, null, 3)}</Text>;
+}
--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,7 +1,36 @@
+import { initTRPC } from "@trpc/server";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import { z } from "zod";
 import express from "express";
+
+export const t = initTRPC.create();
+export const appRouter = t.router({
+	getUser: t.procedure.input(z.string()).query((opts) => {
+		return { id: opts.input, name: "Bilbo" };
+	}),
+	createUser: t.procedure
+		.input(z.object({ name: z.string().min(5) }))
+		.mutation(async (opts) => {
+			return { a: opts.input.name };
+		}),
+});
+
+export type AppRouter = typeof appRouter;
+
+const createContext = ({
+	req,
+	res,
+}: trpcExpress.CreateExpressContextOptions) => ({});
+type Context = Awaited<ReturnType<typeof createContext>>;
 
 const app = express();
 
-app.listen(4000, () => {
-	console.log("Port 4000");
-});
+app.use(
+	"/trpc",
+	trpcExpress.createExpressMiddleware({
+		router: appRouter,
+		createContext,
+	})
+);
+
+app.listen(4000);
